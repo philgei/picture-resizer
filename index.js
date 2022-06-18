aea73d174c28fd05dbd989a098b6b58aab5b83aa
philgei <philipp@geirhos.com>
2022-06-18 19:27:20 +0200

    fixed: everything besides images will be ignored now

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ const compressionRate = 50;
 
 // loop through input folder
 input.forEach((file) => {
-  const test = path.join(__dirname, 'input', file);
-  Jimp.read(test).then((image) => {
+  // if file does not end with jpeg, jpeg, png or gif return
+  if (!file.endsWith('.jpeg') && !file.endsWith('.jpg') && !file.endsWith('.png') && !file.endsWith('.gif')) return;
+
+  const filePath = path.join(__dirname, 'input', file);
+  Jimp.read(filePath).then((image) => {
     const fileNameWithoutExtension = path.basename(file, path.extname(file));
+    // Big Image
     image
       .resize(heightBig, Jimp.AUTO)
       .quality(compressionRate)
       .write(path.join(__dirname, 'output', fileNameWithoutExtension + '_BIG.' + image.getExtension()));
+    // Thumbnail
     image
       .resize(heightThumpnail, Jimp.AUTO)
       .quality(compressionRate)
